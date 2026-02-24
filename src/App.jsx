7658536baf918388a67a22300de0aef9413916b4
omjaisatya <126938261+omjaisatya@users.noreply.github.com>
2026-02-24 15:51:06 +0530

    add trending route and loading lazy add

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@
 import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import NotFound from "./pages/NotFound";
 
 const Home = lazy(() => import("./pages/Home"));
 const Movies = lazy(() => import("./pages/Movies"));
 const MovieDetails = lazy(() => import("./components/MovieDetails"));
 const Loading = lazy(() => import("./components/Loading"));
+const Trending = lazy(() => import("./pages/Trending"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/movie/:id" element={<MovieDetails />} />
+          <Route path="/trending" element={<Trending />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
