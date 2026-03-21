34d67cb6795ba2b712a46241cfdfc6a68f211f6a
omjaisatya <126938261+omjaisatya@users.noreply.github.com>
2026-03-21 14:36:04 +0530

    add tmdb logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -55,7 +55,11 @@ function Footer() {
             rel="noopener noreferrer"
             className="inline-block"
           >
-            <img src="null" alt="TMDB Logo" className="w-20" />
+            <img
+              src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
+              alt="TMDB Logo"
+              className="w-20"
+            />
           </a>
         </div>
       </div>
